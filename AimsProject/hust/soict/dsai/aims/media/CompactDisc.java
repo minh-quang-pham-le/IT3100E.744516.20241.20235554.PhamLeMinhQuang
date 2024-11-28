bacde70a0b4c7bf828928297624d2565ad8d4e02
minh-quang-pham-le <mkw.dsa@gmail.com>
2024-11-28 14:06:24 +0700

    Complete Assignment 5.3

--- AimsProject/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/hust/soict/dsai/aims/media/CompactDisc.java
@@ -1,6 +1,10 @@
 package hust.soict.dsai.aims.media;
+import java.util.ArrayList;
 
 public class CompactDisc extends Disc {
+    private String artist;
+    private ArrayList<Track> tracks;
+
     public CompactDisc(String title) {
         super(title);
     }
@@ -11,5 +15,49 @@ public class CompactDisc extends Disc {
 
     public CompactDisc(String title, String category, float cost) {
         super(title, category, cost);
+    }
+
+    public CompactDisc(String title, String category, String artist, float cost) {
+        super(title, category, cost);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public boolean addTrack(Track track) {
+        if (tracks == null) tracks = new ArrayList<>();
+        if (this.tracks.contains(track)) {
+            System.out.println("The track " + track.getTitle() + " is already in the tracklist of " + this.getTitle());
+            return false;
+        }
+        else {
+            this.tracks.add(track);
+            System.out.println("The track " + track.getTitle() + " is added to the tracklist of " + this.getTitle());
+            return true;
+        }
+    }
+
+    public boolean removeTrack(Track track) {
+        if (this.tracks.remove(track)) {
+            System.out.println("The track " + track.getTitle() + " is removed from the tracklist of " + this.getTitle());
+            return true;
+        }
+        else {
+            System.out.println("The track " + track.getTitle() + " is not in the tracklist of " + this.getTitle());
+            return false;
+        }
+    }
+
+    public int getLength() {
+        if (tracks == null) {
+            tracks = new ArrayList<>();
+        }
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
     }
 }
