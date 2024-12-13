98e35701774b294aee6e1a937b4eca3ff3c0fb4f
minh-quang-pham-le <mkw.dsa@gmail.com>
2024-12-13 18:58:40 +0700

    Update Assignment Lab 5

--- AimsProject/hust/soict/dsai/aims/screen/MediaStore.java
+++ AimsProject/hust/soict/dsai/aims/screen/MediaStore.java
@@ -1,15 +1,7 @@
 package hust.soict.dsai.aims.screen;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
