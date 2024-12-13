39a197a39b7bd44995a0984016421086efa35d96
Minh Kwang <mkw.dsa@gmail.com>
2024-12-13 21:37:26 +0700

    Update Lab 05

--- AimsProject/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -2,14 +2,27 @@ package hust.soict.dsai.aims.screen;
 
 import hust.soict.dsai.aims.cart.Cart.*;
 import hust.soict.dsai.aims.media.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+
 
 public class CartScreenController {
     
     private Cart cart;
+    private boolean filterByID = true;
+    private FilteredList<Media> filteredCart;
 
     @FXML
     private TableView<Media> tblMedia;
@@ -23,6 +36,15 @@ public class CartScreenController {
     @FXML
     private TableColumn<Media, Float> colMediaCost;
 
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private TextField tfFilter;
+
     public CartScreenController(Cart cart) {
         super();
         this.cart = cart;
@@ -31,12 +53,59 @@ public class CartScreenController {
     @FXML
     private void initialize() {
 
-        colMediaTitle.setCellValueFactory(
-                new PropertyValueFactory<Media, String>("title"));
-        colMediacategory.setCellValueFactory(
-                new PropertyValueFactory<Media, String>("category"));
-        colMediaCost.setCellValueFactory(
-                new PropertyValueFactory<Media, Float>("cost"));
+        colMediaTitle.setCellValueFactory(new PropertyValueFactory<Media, String>("title"));
+        colMediacategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
+        colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemsOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Media>() {
+			@Override
+			public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+                if (newValue != null) {
+                    updateButtonBar(newValue);
+                }
+			}	
+		});
+
+        tfFilter.textProperty().addListener(new ChangeListener<String> () {
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				showFilteredMedia(newValue);
+			}
+		});
+    }
+
+    private void updateButtonBar(Media media) {
+		btnRemove.setVisible(true);
+        if (media instanceof Playable) {
+            btnPlay.setVisible(true);
+        }
+        else {
+            btnPlay.setVisible(false);
+        }
+	}
+
+    private void showFilteredMedia(String filter) {
+		if (filter == null || filter.length() == 0) {
+			filteredCart.setPredicate(s -> true);
+		} else {
+			if (filterByID) {
+				try {
+					filteredCart.setPredicate(s -> s.getID() == Integer.parseInt(filter));
+				} catch (NumberFormatException e) {}
+			} else {
+				filteredCart.setPredicate(s -> s.getTitle().toLowerCase().contains(filter));
+			}
+		}
+	}
+
+    @FXML
+    void btnRemovePressed(ActionEvent event) {
+        Media media = tblMedia.getSelectionModel().getSelectedItem();
+        cart.removeMedia(media);
     }
 }
